458f869258ec678f29f064dcebb97d9144ac7eb2
snoyan <epicteam1729@gmail.com>
2022-01-15 21:26:05 +0330

    add searchBox

--- lib/screens/ads/ads_screen.dart
+++ lib/screens/ads/ads_screen.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: implementation_imports, avoid_print, unused_import
 
 import 'package:babysitter/components/person_card.dart';
+import 'package:babysitter/constant.dart';
 import 'package:babysitter/models/nurse.dart';
+import 'package:babysitter/screens/search/search_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/src/provider.dart';
 import '../../data.dart';
@@ -27,11 +29,12 @@ class _AdsScreenState extends State<AdsScreen> {
           children: [
             Padding(
               padding: const EdgeInsets.only(
-                  top: 50.0, left: 100.0, right: 100.0, bottom: 20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
+                  top: 50.0, left: 50.0, right: 50.0, bottom: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                textBaseline: TextBaseline.ideographic,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                children: [
                   Padding(
                     padding: const EdgeInsets.only(bottom: 20.0),
                     child: Text(
@@ -39,31 +42,114 @@ class _AdsScreenState extends State<AdsScreen> {
                       style: const TextStyle(color: Colors.white, fontSize: 40),
                     ),
                   ),
-                  Container(
-                    color: Colors.white,
-                    height: 40.0,
-                    child: Row(
-                      children: [
-                        Flexible(
-                          child: TextField(
-                            cursorColor: Colors.black,
-                            onChanged: (value) {
-                              print(value);
-                            },
-                          ),
+                  Row(
+                    children: [
+                      FloatingActionButton.small(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        onPressed: () {
+                          print('pressed');
+                          showBottomSheet(
+                            context: context,
+                            builder: (context) => const SearchScreen(),
+                          );
+                        },
+                        child: const Icon(
+                          Icons.search,
+                          color: kBaseColor5,
+                          size: 20.0,
                         ),
-                        IconButton(
-                          icon: const Icon(Icons.search),
-                          onPressed: () {
-                            print('pressed');
-                          },
+                      ),
+                      FloatingActionButton.small(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        onPressed: () {
+                          print('pressed');
+                          showBottomSheet(
+                            context: context,
+                            builder: (context) => const SearchScreen(),
+                          );
+                        },
+                        child: const Icon(
+                          Icons.login,
+                          color: kBaseColor5,
+                          size: 20.0,
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
+
+            /// . this part if for search box.
+            // Container(
+            //   height: MediaQuery.of(context).size.height * 0.07,
+            //   decoration: const BoxDecoration(
+            //       color: Colors.white,
+            //       borderRadius: BorderRadius.all(Radius.circular(40))),
+            //   child: Row(
+            //     mainAxisAlignment: MainAxisAlignment.center,
+            //     children: [
+            //       Flexible(
+            //         child: TextField(
+            //           textAlignVertical: TextAlignVertical.bottom,
+            //           decoration: const InputDecoration(
+            //             focusedBorder: OutlineInputBorder(
+            //               // width: 0.0 produces a thin "hairline" border
+            //               borderRadius:
+            //                   BorderRadius.all(Radius.circular(40.0)),
+            //               borderSide:
+            //                   BorderSide(color: Colors.white, width: 0.0),
+            //             ),
+            //             enabledBorder: OutlineInputBorder(
+            //               // width: 0.0 produces a thin "hairline" border
+            //               borderRadius:
+            //                   BorderRadius.all(Radius.circular(40.0)),
+            //               borderSide:
+            //                   BorderSide(color: Colors.white, width: 0.0),
+            //             ),
+            //             disabledBorder: OutlineInputBorder(
+            //               // width: 0.0 produces a thin "hairline" border
+            //               borderRadius:
+            //                   BorderRadius.all(Radius.circular(40.0)),
+            //               borderSide:
+            //                   BorderSide(color: Colors.grey, width: 0.0),
+            //             ),
+            //             hintText: 'جست و جو',
+            //           ),
+            //           textAlign: TextAlign.start,
+            //           cursorColor: Colors.black,
+            //           onChanged: (value) {
+            //             print(value);
+            //           },
+            //         ),
+            //       ),
+            //       Container(
+            //         height: MediaQuery.of(context).size.height * 0.07,
+            //         width: MediaQuery.of(context).size.width * 0.15,
+            //         decoration: const BoxDecoration(
+            //             color: Colors.black,
+            //             borderRadius:
+            //                 BorderRadius.all(Radius.circular(40))),
+            //         child: IconButton(
+            //           icon: const Icon(
+            //             Icons.search,
+            //             color: kBaseColor5,
+            //             size: 20.0,
+            //           ),
+            //           onPressed: () {
+            //             print('pressed');
+            //             showModalBottomSheet(
+            //                 context: context,
+            //                 builder: (context) => Container());
+            //           },
+            //         ),
+            //       ),
+            //     ],
+            //   ),
+            // ),
+
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
--- lib/screens/search/search_screen.dart
+++ lib/screens/search/search_screen.dart
@@ -11,7 +11,7 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height * 0.8,
+      height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width,
       color: kBaseColor5,
       child: Column(
